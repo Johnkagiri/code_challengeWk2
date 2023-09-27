0197dead82a0e4ea638c4f5059d2c056e3e36258
johnkagiri <kagirijohn124@gmail.com>
2023-09-27 10:56:25 +0300

    changed customer

---
+++ customer.py
@@ -0,0 +1,25 @@
+class Customer:
+    all_customers = []
+
+    def __init__(self, given_name, family_name):
+        self.given_name = given_name
+        self.family_name = family_name
+        self.reviews = []
+        Customer.all_customers.append(self)
+
+    def change_given_name(self, new_given_name):
+        self.given_name = new_given_name
+
+    def change_family_name(self, new_family_name):
+        self.family_name = new_family_name
+
+    def full_name(self):
+        return f"{self.given_name} {self.family_name}"
+
+    @classmethod
+    def all(cls):
+        return cls.all_customers
+
+    def add_review(self, restaurant, rating):
+        review = Review(self, restaurant, rating)
+        self.reviews.append(review)
---
+++ restaurant.py
@@ -0,0 +1,24 @@
+class Restaurant:
+    all_restaurants = []
+
+    def __init__(self, name):
+        self.name = name
+        self.reviews = []
+        Restaurant.all_restaurants.append(self)
+
+    def restaurant_name(self):
+        return self.name
+
+    @classmethod
+    def all(cls):
+        return cls.all_restaurants
+
+    def add_review(self, customer, rating):
+        review = Review(customer, self, rating)
+        self.reviews.append(review)
+
+    def reviews(self):
+        return self.reviews
+
+    def customers(self):
+        return list(set([review.customer for review in self.reviews]))
